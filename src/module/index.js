7b35626d72788b904f970e8b5a5aea93e10e0af7
purestart <522382456@qq.com>
2019-09-29 16:48:21 +0800

    update

--- src/module/index.js
+++ src/module/index.js
@@ -9,7 +9,7 @@ module.exports = {
   build: {
     base: baseBuildConfig,
     main: mainBuildConfig,
-    ccm: ccmBuildConfig,
+    // ccm: ccmBuildConfig,
 
     // common: commonBuildConfig
   },
